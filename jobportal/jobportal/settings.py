69115167a02adb7620892d22f8848c1a6b2da554
EC2 Default User <ec2-user@ip-172-31-43-2.eu-west-1.compute.internal>
2024-04-05 18:54:38 +0000

    completed pylint suggested changes

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'jobPortal.urls'
+ROOT_URLCONF = 'jobportal.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'jobPortal.wsgi.application'
+WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 
 # Database
