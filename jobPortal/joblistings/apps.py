d9eda277c963f7ce5c80ac9dbbf27cd0d8ce9740
EC2 Default User <ec2-user@ip-172-31-43-2.eu-west-1.compute.internal>
2024-04-05 13:37:48 +0000

    sonar integration

--- jobPortal/joblistings/apps.py
+++ jobPortal/joblistings/apps.py
@@ -1,5 +1,7 @@
+"""
+This is the apps file of the joblistings
+"""
 from django.apps import AppConfig
-
 
 class JoblistingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
