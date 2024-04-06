6f1929a9b33a80856bbab220a15582226f103cb1
EC2 Default User <ec2-user@ip-172-31-43-2.eu-west-1.compute.internal>
2024-04-06 01:10:33 +0000

    deploy to ebs

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'e734d1d63b2f4592803cb711ec3bd683.vfs.cloud9.eu-west-1.amazonaws.com',
-    'jobsboardenv.eba-dkivrssv.eu-west-1.elasticbeanstalk.com'
+    'jobsboardenv.eba-dkivrssv.eu-west-1.elasticbeanstalk.com',
+    'jobsboard-env.eba-mnh4nzk6.eu-west-2.elasticbeanstalk.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
