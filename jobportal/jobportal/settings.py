aa656fb9fc7de4f6f5bdd9bf498d620edb7237a6
EC2 Default User <ec2-user@ip-172-31-43-2.eu-west-1.compute.internal>
2024-04-06 17:00:18 +0000

    updated settings

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'e734d1d63b2f4592803cb711ec3bd683.vfs.cloud9.eu-west-1.amazonaws.com',
-    'http://jobportal-env.eba-p9uwax9a.eu-west-1.elasticbeanstalk.com'
+    'jobportal-env.eba-p9uwax9a.eu-west-1.elasticbeanstalk.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
